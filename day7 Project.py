c19281146e0dfc4a91a2476bb199a86d6d8613cf
LukeSkyRed <luca_rossi84@hotmail.it>
2021-05-21 16:44:22 +0200

    Add files via upload

---
+++ day7 Project.py
@@ -0,0 +1,12 @@
+#Below you'll find a list which contains the relevant data about a selection of movies.
+#Each item in the list is a tuple containing a movie name and movie budget in that order:
+
+movies = [
+    ("Eternal Sunshine of the Spotless Mind", 20000000),
+    ("Memento", 9000000),
+    ("Requiem for a Dream", 4500000),
+    ("Pirates of the Caribbean: On Stranger Tides", 379000000),
+    ("Avengers: Age of Ultron", 365000000),
+    ("Avengers: Endgame", 356000000),
+    ("Incredibles 2", 200000000)
+]
