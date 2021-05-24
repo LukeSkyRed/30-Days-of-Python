3041f7413e28925e32ecde842917fe6db74c9577
LukeSkyRed <luca_rossi84@hotmail.it>
2021-05-24 12:41:15 +0200

    Add files via upload

--- day8.py
+++ day8.py
@@ -30,13 +30,16 @@ for letter in word:
 #3)Using one of the examples from earlier—or a solution entirely of your own create a program 
 #that prints out every prime number between 1 and 100.
 
+#tips:
+#ELSE clause attached to a loop will only run if a break statement wasn't encountered during the execution of that loop.
+
 for dividend in range(2,101):
     for divisor in range(2, dividend):
         if dividend % divisor == 0:
             break
     else:
         print(f"{dividend} is a prime number")      
-          
+
     
         
 
